3e6ab192fbda470d2dbe2a623a06d08066ed3353
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2026-01-04 22:41:26 +0530

    update

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -1,104 +1,104 @@
-import api from "./api";
+// import api from "./api";
 
 
-export const getAllDoctorsService = async () => {
-    const aToken = localStorage.getItem("aToken");
+// export const getAllDoctorsService = async () => {
+//     const aToken = localStorage.getItem("aToken");
 
-    if (!aToken) {
-        throw new Error("Token not found");
-    }
+//     if (!aToken) {
+//         throw new Error("Token not found");
+//     }
 
-    const resp = await api.post("/admin/all-doctors", {} , {
-        headers: {
-            atoken: aToken
-        }
-    });
+//     const resp = await api.post("/admin/all-doctors", {} , {
+//         headers: {
+//             atoken: aToken
+//         }
+//     });
 
-    return resp.data;
-}
-
-export const changeAvailabilityService = async (docId: string) => {
-    const aToken = localStorage.getItem("aToken");
-
-    if (!aToken) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.post("/admin/change-availability", {docId} , {
-        headers: {
-            atoken: aToken
-        }
-    });
-
-    return resp.data;
-}
-
-export const getAllAppointmentsService = async () => {
-    const aToken = localStorage.getItem("aToken");
-
-    if (!aToken) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.get("/admin/appointments", {
-        headers: {
-            atoken: aToken
-        }
-    })
-
-    return resp.data;
-}
-
-export const adminCancelAppintmentsService = async (appointmentId: string) => {
-    const aToken = localStorage.getItem("aToken");
-
-    if (!aToken) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.post("/admin/cancel-appointment", {appointmentId} , {
-        headers: {
-            atoken: aToken
-        }
-    });
-
-    return resp.data;
-}
-
-export const getDashDataService = async () => {
-    const aToken = localStorage.getItem("aToken");
-
-    if (!aToken) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.get("/admin/dashboard", {
-        headers: {
-            atoken: aToken
-        }
-    })
-
-    return resp.data;
-}
-
-export const addDoctorService = async (formData: FormData) => {
-    const aToken = localStorage.getItem("aToken");
-
-    if (!aToken) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.post("/admin/add-doctor", formData , {
-        headers: {
-            atoken: aToken
-        }
-    });
-
-    return resp.data;
-}
-
-// export const adminLogin = async (email: string , password: string) => {
-//     const resp = await api.post("/admin/login" , {email , password})    // base url athi bawin ethnin passe kotasa methnadi diya hakiya
-
-//     return resp.data
+//     return resp.data;
 // }
+
+// export const changeAvailabilityService = async (docId: string) => {
+//     const aToken = localStorage.getItem("aToken");
+
+//     if (!aToken) {
+//         throw new Error("Token not found");
+//     }
+
+//     const resp = await api.post("/admin/change-availability", {docId} , {
+//         headers: {
+//             atoken: aToken
+//         }
+//     });
+
+//     return resp.data;
+// }
+
+// export const getAllAppointmentsService = async () => {
+//     const aToken = localStorage.getItem("aToken");
+
+//     if (!aToken) {
+//         throw new Error("Token not found");
+//     }
+
+//     const resp = await api.get("/admin/appointments", {
+//         headers: {
+//             atoken: aToken
+//         }
+//     })
+
+//     return resp.data;
+// }
+
+// export const adminCancelAppintmentsService = async (appointmentId: string) => {
+//     const aToken = localStorage.getItem("aToken");
+
+//     if (!aToken) {
+//         throw new Error("Token not found");
+//     }
+
+//     const resp = await api.post("/admin/cancel-appointment", {appointmentId} , {
+//         headers: {
+//             atoken: aToken
+//         }
+//     });
+
+//     return resp.data;
+// }
+
+// export const getDashDataService = async () => {
+//     const aToken = localStorage.getItem("aToken");
+
+//     if (!aToken) {
+//         throw new Error("Token not found");
+//     }
+
+//     const resp = await api.get("/admin/dashboard", {
+//         headers: {
+//             atoken: aToken
+//         }
+//     })
+
+//     return resp.data;
+// }
+
+// export const addDoctorService = async (formData: FormData) => {
+//     const aToken = localStorage.getItem("aToken");
+
+//     if (!aToken) {
+//         throw new Error("Token not found");
+//     }
+
+//     const resp = await api.post("/admin/add-doctor", formData , {
+//         headers: {
+//             atoken: aToken
+//         }
+//     });
+
+//     return resp.data;
+// }
+
+// // export const adminLogin = async (email: string , password: string) => {
+// //     const resp = await api.post("/admin/login" , {email , password})    // base url athi bawin ethnin passe kotasa methnadi diya hakiya
+
+// //     return resp.data
+// // }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,28 +1,146 @@
+// // import api from "./api"
+
+// // export const login = async (username: string , password: string) => {
+// //     const resp = await api.post("/auth/login" , {email: username , password})    // base url athi bawin ethnin passe kotasa methnadi diya hakiya
+
+// //     return resp.data
+// // }
+
+// // export const register = async (username: string , password: string , firstname: string, lastname: string) => {
+// //     const resp = await api.post("/auth/register", {email: username , password, firstname, lastname})
+
+// //     return resp.data
+// // }
+
+// // export const getMyDetails = async () => {
+// //     const resp = await api.get("/auth/me")
+
+// //     return resp.data
+// // }
+
+// // export const refreshTokens = async (refreshToken: string)=>{
+// //     const res = await api.post("/auth/refresh",{
+// //         token:refreshToken
+// //     })
+// //     return res.data
+// // }
+
+// // export const loadUserProfileDataService = async () => {
+
+// //     const token = localStorage.getItem("token");
+
+// //     if (!token) {
+// //         throw new Error("Token not found");
+// //     }
+
+// //     const resp = await api.get("/user/get-profile", {
+// //         headers: {
+// //             token: token
+// //         }
+// //     });
+
+// //     return resp.data;
+// // };
+
+// // export const bookAppointmentService = async (docId: string , slotDate:any, slotTime: any) => {
+// //     const token = localStorage.getItem("token");
+
+// //     if (!token) {
+// //         throw new Error("Token not found");
+// //     }
+
+// //     const resp = await api.post('/user/book-appointment' , {docId, slotDate, slotTime } , {headers: { token }} )
+// //     return resp.data;
+// // }
+
+// // export const getUserAppointmentsService = async () => {
+
+// //     const token = localStorage.getItem("token");
+
+// //     if (!token) {
+// //         throw new Error("Token not found");
+// //     }
+
+// //     const resp = await api.get("/user/appointments" , {
+// //         headers: {
+// //             token: token
+// //         }
+// //     })
+
+// //     return resp.data;
+// // }
+
+// // export const cancelAppointmentService = async (appointmentId: string) => {
+
+// //     const token = localStorage.getItem("token");
+
+// //     if (!token) {
+// //         throw new Error("Token not found");
+// //     }
+
+// //     const resp = await api.post("/user/cancel-appointment" , {appointmentId} ,{
+// //         headers: {
+// //             token: token
+// //         }
+// //     })
+
+// //     return resp.data;
+// // }
+
+// // export const updateUserDataService = async (formData: FormData) => {
+// //     const token = localStorage.getItem("token");
+
+// //     if (!token) {
+// //         throw new Error("Token not found");
+// //     }
+
+// //     const resp = await api.post("/user/update-profile" , formData ,{
+// //         headers: {
+// //             token: token
+// //         }
+// //     })
+
+// //     return resp.data;
+// // }
+
+// // // export const refreshTokens = async (refreshToken: string) => {
+// // //     const resp = await api.post("/user/refresh", { token: refreshToken })
+// // //     return resp.data
+// // // }
+
+// // export const sendPasswordResetEmail = async (email: string) => {
+    
+// //     const resp = await api.post("/user/forget-password", { email })
+    
+// //     return resp.data
+// // }
+
+// // // export const resetPassword = async (token: any, newPassword: string) => {
+// // //     const resp = await api.put(`/user/reset-password/${token}`, { newPassword });
+// // //     return resp.data;
+// // // }
+
+// // export const resetPassword = async (token: string, newPassword: string) => {
+// //     const resp = await api.put(
+// //         `/user/reset-password`,
+// //         { newPassword },
+// //         { headers: { "x-reset-token": token } }
+// //     );
+// //     return resp.data;
+// // };
 // import api from "./api"
 
-// export const login = async (username: string , password: string) => {
-//     const resp = await api.post("/auth/login" , {email: username , password})    // base url athi bawin ethnin passe kotasa methnadi diya hakiya
+// export const register = async (name: string , email: string , password: string ) => {
+//     const resp = await api.post("/user/register", { name, email, password })
 
 //     return resp.data
 // }
 
-// export const register = async (username: string , password: string , firstname: string, lastname: string) => {
-//     const resp = await api.post("/auth/register", {email: username , password, firstname, lastname})
+
+// export const login = async (email: string , password: string) => {
+//     const resp = await api.post("/user/login" , {email , password})    // base url athi bawin ethnin passe kotasa methnadi diya hakiya
 
 //     return resp.data
-// }
-
-// export const getMyDetails = async () => {
-//     const resp = await api.get("/auth/me")
-
-//     return resp.data
-// }
-
-// export const refreshTokens = async (refreshToken: string)=>{
-//     const res = await api.post("/auth/refresh",{
-//         token:refreshToken
-//     })
-//     return res.data
 // }
 
 // export const loadUserProfileDataService = async () => {
@@ -103,10 +221,10 @@
 //     return resp.data;
 // }
 
-// // export const refreshTokens = async (refreshToken: string) => {
-// //     const resp = await api.post("/user/refresh", { token: refreshToken })
-// //     return resp.data
-// // }
+// export const refreshTokens = async (refreshToken: string) => {
+//     const resp = await api.post("/user/refresh", { token: refreshToken })
+//     return resp.data
+// }
 
 // export const sendPasswordResetEmail = async (email: string) => {
     
@@ -128,121 +246,3 @@
 //     );
 //     return resp.data;
 // };
-import api from "./api"
-
-export const register = async (name: string , email: string , password: string ) => {
-    const resp = await api.post("/user/register", { name, email, password })
-
-    return resp.data
-}
-
-
-export const login = async (email: string , password: string) => {
-    const resp = await api.post("/user/login" , {email , password})    // base url athi bawin ethnin passe kotasa methnadi diya hakiya
-
-    return resp.data
-}
-
-export const loadUserProfileDataService = async () => {
-
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.get("/user/get-profile", {
-        headers: {
-            token: token
-        }
-    });
-
-    return resp.data;
-};
-
-export const bookAppointmentService = async (docId: string , slotDate:any, slotTime: any) => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.post('/user/book-appointment' , {docId, slotDate, slotTime } , {headers: { token }} )
-    return resp.data;
-}
-
-export const getUserAppointmentsService = async () => {
-
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.get("/user/appointments" , {
-        headers: {
-            token: token
-        }
-    })
-
-    return resp.data;
-}
-
-export const cancelAppointmentService = async (appointmentId: string) => {
-
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.post("/user/cancel-appointment" , {appointmentId} ,{
-        headers: {
-            token: token
-        }
-    })
-
-    return resp.data;
-}
-
-export const updateUserDataService = async (formData: FormData) => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        throw new Error("Token not found");
-    }
-
-    const resp = await api.post("/user/update-profile" , formData ,{
-        headers: {
-            token: token
-        }
-    })
-
-    return resp.data;
-}
-
-export const refreshTokens = async (refreshToken: string) => {
-    const resp = await api.post("/user/refresh", { token: refreshToken })
-    return resp.data
-}
-
-export const sendPasswordResetEmail = async (email: string) => {
-    
-    const resp = await api.post("/user/forget-password", { email })
-    
-    return resp.data
-}
-
-// export const resetPassword = async (token: any, newPassword: string) => {
-//     const resp = await api.put(`/user/reset-password/${token}`, { newPassword });
-//     return resp.data;
-// }
-
-export const resetPassword = async (token: string, newPassword: string) => {
-    const resp = await api.put(
-        `/user/reset-password`,
-        { newPassword },
-        { headers: { "x-reset-token": token } }
-    );
-    return resp.data;
-};
